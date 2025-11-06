e79ae2e17cd7d9f17989910edaae85bc0ef94354
Imam Rizky Pratama <140227265+imamrpratama@users.noreply.github.com>
2025-11-06 21:28:34 +0800

    Update : change image path

--- app/page.tsx
+++ app/page.tsx
@@ -100,9 +100,9 @@ const Portfolio = () => {
       description:
         "This project is built using Laravel, PHP, MySQL, and Tailwind CSS. It powers Eduvice, a web platform that enables users to donate, track, and manage electronic devices to be refurbished and distributed to students in need, promoting digital inclusion and reducing electronic waste.",
       images: [
-        "./images/eduvice_1.png",
-        "./images/eduvice_2.png",
-        "./images/eduvice_3.png",
+        "/portfolio/images/eduvice_1.png",
+        "/portfolio/images/eduvice_2.png",
+        "/portfolio/images/eduvice_3.png",
       ],
       demo: "https://eduvice.id",
     },
@@ -113,9 +113,9 @@ const Portfolio = () => {
       description:
         "This website is built using Laravel, MySQL, and Tailwind CSS, delivering a modern, responsive, and secure web platform. It powers the Indonesia Sustainability Forum site, providing information about the event and enabling users to register as participants seamlessly.",
       images: [
-        "./images/isf_1.png",
-        "./images/isf_2.png",
-        "./images/isf_3.png",
+        "/portfolio/images/isf_1.png",
+        "/portfolio/images/isf_2.png",
+        "/portfolio/images/isf_3.png",
       ],
       demo: "https://indonesiasustainabilityforum.co.id/",
     },
@@ -126,9 +126,9 @@ const Portfolio = () => {
       description:
         "Built using Laravel, REST API, and MS SQL, designed to streamline organizational asset tracking and maintenance. The Asset Management System enables users to efficiently manage asset records, monitor conditions, and automate maintenance scheduling.",
       images: [
-        "./images/asset_1.png",
-        "./images/asset_2.png",
-        "./images/asset_3.png",
+        "/portfolio/images/asset_1.png",
+        "/portfolio/images/asset_2.png",
+        "/portfolio/images/asset_3.png",
       ],
       demo: "https://demo.example.com",
     },
@@ -138,7 +138,10 @@ const Portfolio = () => {
       tech: ["Laravel", "REST API", "Tailwind CSS", "Xendit"],
       description:
         "Developed with Laravel, REST API, and MS SQL, this system serves as a comprehensive villa booking platform integrated with asset management data. It enables users to reserve villas, track booking status, and complete payments securely through the Xendit payment gateway.",
-      images: ["./images/villa_1.png", "./images/villa_2.png"],
+      images: [
+        "/portfolio/images/villa_1.png",
+        "/portfolio/images/villa_2.png",
+      ],
       demo: "https://villaberastagimas.com",
     },
     {
@@ -147,7 +150,7 @@ const Portfolio = () => {
       tech: ["Flutter", "REST API", "Firebase Cloud Messaging"],
       description:
         "Built with Flutter and integrated through REST API with the Asset Management System, this mobile application enables users to perform assigned tasks and submit real-time proof such as photos and locations. Designed for field operations, it streamlines task tracking, verification, and reporting, ensuring transparency and accountability across maintenance and asset management activities.",
-      images: ["./images/fama.png"],
+      images: ["/portfolio/images/fama.png"],
       demo: "https://play.google.com/store/apps/details?id=com.assets_management.fama&hl=en",
     },
     {
@@ -156,7 +159,10 @@ const Portfolio = () => {
       tech: ["Laravel", "Google Drive API", "Automation"],
       description:
         "Developed with Laravel, this project automates the process of backing up files to Google Drive with integrated email notifications and error handling. It allows users to schedule and manage backups directly from the Laravel console, ensuring secure data storage, simplified recovery, and reliable automation for maintaining critical project files.",
-      images: ["./images/gdrive_1.png", "./images/gdrive_2.png"],
+      images: [
+        "/portfolio/images/gdrive_1.png",
+        "/portfolio/images/gdrive_2.png",
+      ],
 
       demo: "https://github.com/imamrpratama/automation-gdrive-backup",
     },
@@ -166,7 +172,10 @@ const Portfolio = () => {
       tech: ["Next.js", "Tailwind CSS", "WhatsApp API"],
       description:
         "Built with Next.js, this website serves as a company profile platform for Harmoni Rent Car, showcasing available vehicles and rental services. It features a clean, responsive design where users can browse cars, view details, and make bookings easily via WhatsApp, providing a simple and direct communication channel for customers.",
-      images: ["./images/harmoni_1.png", "./images/harmoni_2.png"],
+      images: [
+        "/portfolio/images/harmoni_1.png",
+        "/portfolio/images/harmoni_2.png",
+      ],
       demo: "https://harmonirentcar.com",
     },
   ];
